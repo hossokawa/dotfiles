c8da4e566893d7eca3910c0e0ca3faae99164d0e
hossokawa <43284591+hossokawa@users.noreply.github.com>
2025-10-19 18:52:44 -0300

    nvim: update config for lazyvim v15

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,29 +1,11 @@
 return {
   {
-    "williamboman/mason.nvim",
-    version = "^1.0.0",
-    opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, {
-        "shellcheck",
-        "shfmt",
-        -- Golang
-        "gopls",
-        "gofumpt",
-        "goimports",
-        -- Python
-        "basedpyright",
-        "ruff",
-      })
-    end,
-  },
-  {
     "neovim/nvim-lspconfig",
     opts = {
       inlay_hints = { enabled = false },
       diagnostics = {
         virtual_text = false,
       },
-      ---@type lspconfig.options
       servers = {
         basedpyright = {
           settings = {
@@ -65,58 +47,7 @@ return {
             },
           },
         },
-        tsserver = {
-          settings = {
-            typescript = {
-              inlayHints = {
-                includeInlayParameterNameHints = "none",
-                includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-                includeInlayFunctionParameterTypeHints = false,
-                includeInlayVariableTypeHints = false,
-                includeInlayPropertyDeclarationTypeHints = false,
-                includeInlayFunctionLikeReturnTypeHints = false,
-                includeInlayEnumMemberValueHints = false,
-              },
-            },
-            javascript = {
-              inlayHints = {
-                includeInlayParameterNameHints = "none",
-                includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-                includeInlayFunctionParameterTypeHints = false,
-                includeInlayVariableTypeHints = false,
-                includeInlayPropertyDeclarationTypeHints = false,
-                includeInlayFunctionLikeReturnTypeHints = false,
-                includeInlayEnumMemberValueHints = false,
-              },
-            },
-          },
-        },
-      },
-      setup = {},
-    },
-  },
-  {
-    "mfussenegger/nvim-lint",
-    opts = {
-      linters_by_ft = {
-        markdown = {},
       },
     },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    opts = function()
-      local keys = require("lazyvim.plugins.lsp.keymaps").get()
-      vim.list_extend(keys, {
-        {
-          "gd",
-          function()
-            require("telescope.builtin").lsp_definitions({ reuse_win = false })
-          end,
-          desc = "Goto Definition",
-          has = "definition",
-        },
-      })
-    end,
   },
 }
